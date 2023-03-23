df69075b931a42cf369efb64af6dd942f01b743b
hjhjhjkh <115256231+hjhjhjkh@users.noreply.github.com>
2023-03-23 12:39:19 +0100

    Update main.R

--- main.R
+++ main.R
@@ -14,8 +14,8 @@ library(writexl)
 procitanXls <- read_excel("data/uNizUlazA.xls", col_names = FALSE)
 myList <- list()
 
-for(k in 1:50){
-  for (r in 1:10){ # 10:1 za negativne brojeve; 1:10 za pozitivne brojeve; voditi racuna u proveri i da negativna nula nema u (zato nema tacno 1000 nego 999 za razliku od tabele)
+for(r in 1:50){ # 1:nrow(procitanXls) i 1:ncol(procitanXls) u opstem slucaju; ovde specificno menjam smer citanja tabele na pola niza pa zato tacan broj
+  for (k in 1:10){ # 10:1 za negativne brojeve; 1:10 za pozitivne brojeve; voditi racuna u proveri i da negativna nula nema u (zato nema tacno 1000 nego 999 za razliku od tabele)
     myList <- append(myList, procitanXls[k,r])
   }
 }
